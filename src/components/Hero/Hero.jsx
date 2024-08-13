ea30601c10e1dd6ae81081ee6d6d71f434fe0468
rajendrahinge <rajendrahinge24@gmail.com>
2024-08-14 03:52:03 +0530

    fix

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,9 +4,9 @@ import heroImage from "../../assets/hero.svg";
 const Hero = () => {
   return (
     <div className={styles.hero}>
-      <div>
-        <h1> 100 Thousand Songs, ad - free </h1>
-        <h1> Over thousands podcast episodes</h1>
+      <div className={styles.hbg}>
+        <h1>100 Thousand Songs, ad - free</h1>
+        <h1>Over thousands podcast episodes</h1>
       </div>
       <img src={heroImage} alt="Hero_Image" />
     </div>
